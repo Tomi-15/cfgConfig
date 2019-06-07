e655103a0fc56facf06129cd873eb437b50c09af
Tomas Wegenast <tomaswegenast@gmail.com>
2019-06-07 16:32:26 -0300

    Added Serialization helpers

---
+++ src/cfgConfig.Core/Serialization/BinarySerializer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace cfgConfig.Core.Serialization
+{
+    internal static class BinarySerializer
+    {
+        public static T Deserialize<T>(string input)
+        {
+            byte[] b = Convert.FromBase64String(input);
+            using (var stream = new MemoryStream(b))
+            {
+                var formatter = new BinaryFormatter();
+                stream.Seek(0, SeekOrigin.Begin);
+                return (T)formatter.Deserialize(stream);
+            }
+        }
+
+        public static object Deserialize(string input)
+        {
+            byte[] b = Convert.FromBase64String(input);
+            using (var stream = new MemoryStream(b))
+            {
+                var formatter = new BinaryFormatter();
+                stream.Seek(0, SeekOrigin.Begin);
+                return formatter.Deserialize(stream);
+            }
+        }
+
+        public static string Serialize<T>(T settings)
+        {
+            using (var stream = new MemoryStream())
+            {
+                var formatter = new BinaryFormatter();
+                formatter.Serialize(stream, settings);
+                stream.Flush();
+                stream.Position = 0;
+                return Convert.ToBase64String(stream.ToArray());
+            }
+        }
+    }
+}
---
+++ src/cfgConfig.Core/Serialization/XmlSerializer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Xml;
+
+namespace cfgConfig.Core.Serialization
+{
+    internal static class XmlSerializer
+    {
+        public static T Deserialize<T>(string content)
+        {
+            var serializer = new System.Xml.Serialization.XmlSerializer(typeof(T));
+
+            using (MemoryStream ms = new MemoryStream(Encoding.UTF8.GetBytes(content)))
+                return (T)serializer.Deserialize(ms);
+        }
+
+        public static object Deserialize(string content, Type type)
+        {
+            var serializer = new System.Xml.Serialization.XmlSerializer(type);
+
+            using (MemoryStream ms = new MemoryStream(Encoding.UTF8.GetBytes(content)))
+                return serializer.Deserialize(ms);
+        }
+
+        public static string Serialize(object obj)
+        {
+            var serializer = new System.Xml.Serialization.XmlSerializer(typeof(object));
+
+            using(var sw = new StringWriter())
+            using(XmlWriter xml = XmlWriter.Create(sw))
+            {
+                serializer.Serialize(sw, obj);
+                return sw.ToString();
+            }
+        }
+    }
+}
