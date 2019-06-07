fb5c6334104be9fe2030b4c42a3c95353d204c9a
Tomas Wegenast <tomaswegenast@gmail.com>
2019-06-07 16:32:33 -0300

    Improved

---
+++ src/cfgConfig.Core/Extensions/ConfigurationExtensions.cs
@@ -0,0 +1,28 @@
+﻿using cfgConfig.Core.Exceptions;
+
+namespace cfgConfig.Core.Extensions
+{
+    /// <summary>
+    /// Extensions methods to work with configurations
+    /// </summary>
+    public static class ConfigurationExtensions
+    {
+        /// <summary>
+        /// Saves an specific configuration type
+        /// </summary>
+        /// <typeparam name="TConfig">The configuration type to save</typeparam>
+        /// <param name="config">The configuration to save</param>
+        public static void SaveConfig<TConfig>(this TConfig config)
+        {
+            // Get manager
+            var manager = ConfigurationManager.GetManagerWhoImplements<TConfig>();
+
+            // If null, throw exception
+            if (manager == null)
+                throw new ConfigNotFoundException($"Configuration of type {typeof(TConfig).ToString()} not found.");
+
+            // Save implementation
+            manager.Implementations.SaveImplementation<TConfig>();
+        }
+    }
+}
