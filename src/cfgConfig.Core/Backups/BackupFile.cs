462508194a722826f175dca0dd9d6b64f662701b
Tomas Wegenast <tomaswegenast@gmail.com>
2019-06-09 00:14:57 -0300

    Added backup

---
+++ src/cfgConfig.Core/Backups/BackupFile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace cfgConfig.Core.Backups
+{
+    internal class BackupFile
+    {
+        /// <summary>
+        /// The path to the file.
+        /// </summary>
+        public string Path { get; set; }
+
+        /// <summary>
+        /// The date when the backup file was created
+        /// </summary>
+        public DateTime CreatedDate { get; set; }
+    }
+}
